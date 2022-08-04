0526a6d3a6d00aaaf782bb77e3d9da4ddd25d6f7
copenhagen2 <copenhagen@sjtu.edu.cn>
2022-08-04 23:55:36 +0800

    update the maze

--- python_version/src/main.py
+++ python_version/src/main.py
@@ -10,7 +10,7 @@ import A_star
 import visualization
 
 if __name__  == "__main__":
-    N = 40
+    N = 50
     map = np.zeros((N, N))
     
     # Mark the border of the map
@@ -23,14 +23,16 @@ if __name__  == "__main__":
         map[i][N-6] = 5
 
     # Mark the obstacles randomly
-    for i in range(300):
-        x = random.randint(6,N-6)
-        y = random.randint(6,N-6)
-        map[x][y] = 5
-    
+    for i in range(5,27):
+        map[i][17] = 5
+    for i in range(43,18,-1):
+        map[i][35] = 5
+    for i in range(30,40):
+        map[19][i] = 5
+
     # mark the start point and the end point
-    s = (random.randint(6,N-7), random.randint(6,N-7))
-    t = (random.randint(6,N-7), random.randint(6,N-7))
+    s = (10,10)
+    t = (40,40)
     map[s[0]][s[1]] = 1
     map[t[0]][t[1]] = 2  
 
@@ -43,13 +45,14 @@ if __name__  == "__main__":
     map_copy_3 = np.array(map)
 
     m, searched, path, cost = A_star.A_star(map_1, s, t, N)
-    visualization.animation_save(map_copy_1,searched,path,r'G:\Summer Lab\path_planning\python_version\res\A_star.gif')
     print(f"A*: {cost}")
-
+    visualization.animation_save(map_copy_1,searched,path,r'G:\Summer Lab\path_planning\python_version\res\A_star.gif')
+    
     m, searched, path, cost = bfs.bfs(map_2, s, t, N)
-    visualization.animation_save(map_copy_2,searched,path,r'G:\Summer Lab\path_planning\python_version\res\bfs.gif')
     print(f"bfs: {cost}")
-
+    visualization.animation_save(map_copy_2,searched,path,r'G:\Summer Lab\path_planning\python_version\res\bfs.gif')
+    
     m, searched, path, cost = dfs.dfs(map_3, s, t, N)
-    visualization.animation_save(map_copy_3,searched,path,r'G:\Summer Lab\path_planning\python_version\res\dfs.gif')
     print(f"dfs: {cost}")
+    visualization.animation_save(map_copy_3,searched,path,r'G:\Summer Lab\path_planning\python_version\res\dfs.gif')
+    
